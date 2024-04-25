952e427231df9b34e730257b7295439013e71ce3
Kilian Boute <kilian.boute@gmail.com>
2024-04-25 14:40:10 +0200

    Add Jest test run to github workflow

---
+++ ui/__tests__/Home.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "../src/app/page.tsx";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
