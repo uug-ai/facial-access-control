bde7520dc2dd06811400ec8da2d1c27fd3a12e6f
Cedric Verstraeten <cedric@verstraeten.io>
2024-05-19 00:47:24 +0200

    cleanup + fix import uug-ai/ui

--- ui/postcss.config.mjs
+++ ui/postcss.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
