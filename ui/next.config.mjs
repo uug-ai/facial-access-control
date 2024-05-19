0485807336673dbfbd1178996d69c41ab010f72d
Cedric Verstraeten <cedric@verstraeten.io>
2024-05-19 19:39:00 +0200

    add fix for basepath (github pages)

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+    basePath: "/facial-access-control",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
