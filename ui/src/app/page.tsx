85046526ff5ab6639d4d56507d855f76be0d3b8e
Cedric Verstraeten <cedric@verstraeten.io>
2024-05-19 17:26:06 +0200

    remove storybook + fix tests (show heading)

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Facial Access Control</h1>
       <Gradient />
       <Button>Click me</Button>
       <Input placeholder="Type here" />
