1d66f2351ed6c58213a4307660936e0b74277f9d
Cedric Verstraeten <cedric@verstraeten.io>
2024-05-19 19:45:26 +0200

    disable basepath as we will deploy on base domain "head.uug.ai"

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/facial-access-control",
+    // basePath: "/facial-access-control", <==== required for github pages only.
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
