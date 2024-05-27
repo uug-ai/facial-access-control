2fd72a3ad10e370a6062ed43ff27b3ebdaf518a4
Kilian <kilian.boute@gmail.com>
2024-05-27 15:04:12 +0200

    Fix GetLocation redundancy issue

--- api/controllers/locations.go
+++ api/controllers/locations.go
@@ -12,9 +12,9 @@ import "github.com/uug-ai/facial-access-control/api/models"
 func GetLocations() []models.Location {
 	// Generate random locations and return results
 	locations := []models.Location{
-		models.Location{Id: 1, Name: "Location 1", Address: "Address 1", Lat: 1.0, Lng: 1.0},
-		models.Location{Id: 2, Name: "Location 2", Address: "Address 2", Lat: 2.0, Lng: 2.0},
-		models.Location{Id: 3, Name: "Location 3", Address: "Address 3", Lat: 3.0, Lng: 3.0},
+		{Id: 1, Name: "Location 1", Address: "Address 1", Lat: 1.0, Lng: 1.0},
+		{Id: 2, Name: "Location 2", Address: "Address 2", Lat: 2.0, Lng: 2.0},
+		{Id: 3, Name: "Location 3", Address: "Address 3", Lat: 3.0, Lng: 3.0},
 	}
 	return locations
 }
