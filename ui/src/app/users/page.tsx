03dcc7f5f5f07c554def1e166df342688b717dd3
Kilian <kilian.boute@gmail.com>
2024-05-30 14:52:59 +0200

    Add /users error handling

--- ui/src/app/users/page.tsx
+++ ui/src/app/users/page.tsx
@@ -7,6 +7,35 @@ export default function Users() {
   const { data, error, isLoading } = useGetUsersQuery("/users");
 
   console.log(data);
+  console.log(error);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Possible values of error:
+  // FetchBaseQueryError | SerializedError | undefined
+  // 1) Checking if error is NOT undefined:
+  if (error) {
+    // 2) Checking if error is FetchBaseQueryError based on
+    // discriminated property 'status':
+    if ("status" in error) {
+      // you can access all properties of `FetchBaseQueryError` here
+      const errMsg =
+        "error" in error ? error.error : JSON.stringify(error.data);
+
+      return (
+        <div>
+          <div>An error has occurred:</div>
+          <div>{errMsg}</div>
+        </div>
+      );
+      // 3) We're left with the 3rd case, SerializedError:
+    } else {
+      // you can access all properties of `SerializedError` here
+      return <div>{error.message}</div>;
+    }
+  }
 
   return (
     <div>
