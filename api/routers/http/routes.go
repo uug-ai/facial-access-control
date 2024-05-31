5151821f65981c223053b0848c4bcb953919c55b
Kilian <kilian.boute@gmail.com>
2024-05-31 11:19:03 +0200

    Make getUser get a dynamic id

--- api/routers/http/routes.go
+++ api/routers/http/routes.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"strconv"
+
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/uug-ai/facial-access-control/api/controllers"
@@ -35,8 +37,21 @@ func AddRoutes(r *gin.Engine, authMiddleware *jwt.GinJWTMiddleware) *gin.RouterG
 		})
 
 		api.GET("/users/:id", func(c *gin.Context) {
-			// Create a list of random users
-			user := controllers.GetUser(1)
+			// Get the id parameter from the URL
+			id := c.Param("id")
+
+			// Convert id to an integer
+			userID, err := strconv.Atoi(id)
+			if err != nil {
+				c.JSON(400, gin.H{
+					"error": "Invalid user ID",
+				})
+				return
+			}
+
+			// Use the userID to fetch the user
+			user := controllers.GetUser(userID)
+
 			c.JSON(200, gin.H{
 				"data": user,
 			})
