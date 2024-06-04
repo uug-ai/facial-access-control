3cdfd0ca2d7486cc75cb28a2d77a099e4a1b88b2
Kilian <kilian.boute@gmail.com>
2024-06-04 14:23:26 +0200

    Revert login page

--- ui/src/app/login/page.tsx
+++ ui/src/app/login/page.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import {
   Button,
   Input,
@@ -10,32 +9,9 @@ import {
   Password,
   Row,
 } from "../../components/ui";
-import React, { FormEvent } from "react";
+import React from "react";
 
 export default function Login() {
-  const router = useRouter();
-
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = new FormData(event.currentTarget);
-    const email = form.get("email") as string;
-    const password = form.get("password") as string;
-
-    const response = await fetch("/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    if (response.ok) {
-      router.push("/dashboard");
-    } else {
-      alert("Failed to login");
-    }
-  }
-
   return (
     <Box className="shadow-inner min-h-screen min-w-fit max-w-96 w-4/12 flex flex-col items-start bg-primary-50/15">
       <Gradient />
@@ -43,24 +19,22 @@ export default function Login() {
         <Text as="h2" size="5xl" weight="semibold" className="pb-8">
           Sign in
         </Text>
-        <form onSubmit={handleSubmit}>
-          <Text as="label" for="email" weight="semibold" className="mb-1">
-            Email
+        <Text as="label" for="email" weight="semibold" className="mb-1">
+          Email
+        </Text>
+        <Input type="text" id="email" placeholder="email" className="mb-4" />
+        <Row className="mb justify-between ">
+          <Text as="label" for="password" weight="semibold">
+            Password
           </Text>
-          <Input type="text" id="email" placeholder="email" className="mb-4" />
-          <Row className="mb justify-between ">
-            <Text as="label" for="password" weight="semibold">
-              Password
-            </Text>
-            <Text as="a" variant="link" color="light">
-              forgot password?
-            </Text>
-          </Row>
-          <Password placeholder="password" id="username" className="mb-4" />
-          <Button type="submit" variant="solid" width="third">
-            Sign in
-          </Button>
-        </form>
+          <Text as="a" variant="link" color="light">
+            forgot password?
+          </Text>
+        </Row>
+        <Password placeholder="password" id="username" className="mb-4" />
+        <Button type="submit" variant="solid" width="third">
+          Sign in
+        </Button>
       </Stack>
       <Socials className="mt-auto self-center justify-self-end p-8" />
     </Box>
