f3d11f775fdea856110a4050a808d2176216233f
Kilian <kilian.boute@gmail.com>
2024-06-06 15:04:44 +0200

    Fix dashboard

--- ui/src/app/dashboard/page.tsx
+++ ui/src/app/dashboard/page.tsx
@@ -4,35 +4,32 @@ import AddUser from "./components/AddUser";
 import UserTable from "./components/UserTable";
 import LocationInfo from "./components/LocationInfo";
 import AddLocation from "./components/AddLocation";
-import { AuthWrapper } from "@/components/AuthWrapper";
 
 const Dashboard = () => {
   return (
-    <AuthWrapper>
-      <Box>
-        <Gradient />
-        <Stack className="gap-12 p-8 ">
-          <Row className="justify-between">
-            <Text as="h1" size="5xl" weight="semibold">
-              Dashboard
-            </Text>
-            <Text as="h2" size="4xl" weight="semibold" className="self-end">
-              Organisation
-            </Text>
+    <Box>
+      <Gradient />
+      <Stack className="gap-12 p-8 ">
+        <Row className="justify-between">
+          <Text as="h1" size="5xl" weight="semibold">
+            Dashboard
+          </Text>
+          <Text as="h2" size="4xl" weight="semibold" className="self-end">
+            Organisation
+          </Text>
+        </Row>
+        <Row className="justify-between gap-4">
+          <Row>
+            <LocationInfo />
           </Row>
-          <Row className="justify-between gap-4">
-            <Row>
-              <LocationInfo />
-            </Row>
-            <Row className="gap-4 justify-end">
-              <AddLocation />
-              <AddUser />
-            </Row>
+          <Row className="gap-4 justify-end">
+            <AddLocation />
+            <AddUser />
           </Row>
-          <UserTable />
-        </Stack>
-      </Box>
-    </AuthWrapper>
+        </Row>
+        <UserTable />
+      </Stack>
+    </Box>
   );
 };
 
