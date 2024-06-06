66202f98471b9574ef9efa5005ea38a0522f7f1e
Kilian <kilian.boute@gmail.com>
2024-06-06 17:12:03 +0200

    Add NextAuth configs

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import "../index.css";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <StoreProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+      <SessionProvider>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </SessionProvider>
     </StoreProvider>
   );
 }
--- ui/src/app/login/page.tsx
+++ ui/src/app/login/page.tsx
@@ -19,26 +19,5 @@ export default function Login() {
       <LoginForm />
       <Socials className="mt-auto self-center justify-self-end p-8" />
     </Box>
-
-    // <Box className="shadow-inner min-h-screen min-w-fit max-w-96 w-4/12 flex flex-col items-start bg-primary-50/15">
-    //   <Gradient />
-    //   <Stack className="p-8">
-    //     <Text as="h2" size="5xl" weight="semibold">
-    //       Sign in
-    //     </Text>
-    //     <Input
-    //       type="text"
-    //       id="username"
-    //       placeholder="username"
-    //       className="mb-4"
-    //     />
-    //     <Row className="mb-1"></Row>
-    //     <Password placeholder="password" id="username" className="mb-4" />
-    //     <Button type="submit" variant="solid" width="third">
-    //       Sign in
-    //     </Button>
-    //   </Stack>
-    //   <Socials className="mt-auto self-center justify-self-end p-8" />
-    // </Box>
   );
 }
