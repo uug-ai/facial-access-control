1ef82ac064f12e53d1d7861ce64bb2c31c6d912d
Kilian <kilian.boute@gmail.com>
2024-06-21 13:28:02 +0200

    Add Dockerfile and dockerignore

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -4,7 +4,7 @@
 const nextConfig = {
   // basePath: "/facial-access-control", <==== required for github pages only.
   // output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  reactStrictMode: true,docker
 };
 
 export default nextConfig;
