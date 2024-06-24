bba50475ea3a2bac332546420897895c225d5b9d
Luis Maja <146820631+LuisMaja@users.noreply.github.com>
2024-06-24 13:41:37 +0200

    Create sections for location page

---
+++ ui/src/app/location/page.tsx
@@ -0,0 +1,32 @@
+import { Box, Stack } from "../../components/ui";
+
+const Location = () => (
+    <Stack>
+    <Header/>
+    <Table/>
+    <LocationOverview/>
+    </Stack>
+
+)
+
+const Header = () => (
+      <Box>
+
+      </Box>
+  );
+
+  const Table = () => (
+    <Box>
+
+    </Box>
+);
+
+const LocationOverview = () => (
+    <Box>
+
+    </Box>
+);
+
+
+
+  export default Location
