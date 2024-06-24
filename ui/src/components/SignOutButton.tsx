b3e665f777bd63f6359fc2c3b99a0639f05a9bcd
Kilian <kilian.boute@gmail.com>
2024-06-24 14:41:03 +0200

    Update nextauth url in env

--- ui/src/components/SignOutButton.tsx
+++ ui/src/components/SignOutButton.tsx
@@ -4,8 +4,6 @@ import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function SignOutButton() {
-  const router = useRouter();
-
   const handleSignOut = () => {
     signOut({
       callbackUrl: "/signin",
