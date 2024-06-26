dfb6ed2f1497a3f1fb9d6b8a7b479479d818de27
Kilian <kilian.boute@gmail.com>
2024-06-26 14:45:09 +0200

    Adjust dashboard layout

--- ui/src/app/dashboard/layout.tsx
+++ ui/src/app/dashboard/layout.tsx
@@ -1,21 +1,15 @@
 import Navigation from "@/components/Navigation";
-import SignOutButton from "@/components/SignOutButton";
-import { Row, Stack } from "../../components/ui";
-import { authOptions } from "@/utils/auth";
-import { getServerSession } from "next-auth";
+import { Row } from "@/components/ui";
 
 export default async function DashboardLayout({
-  children, // will be a page or nested layout
+  children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
   return (
-    <section>
-      <Row>
-        <Navigation />
-        <main className="grow">{children}</main>
-      </Row>
-    </section>
+    <Row>
+      <Navigation />
+      <main className="grow">{children}</main>
+    </Row>
   );
 }
